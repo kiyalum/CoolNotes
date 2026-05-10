034a5c5da5074b017e1299ec320cbae7f9f1f58b
Serhii <serhii@example.com>
2026-05-10 22:28:26 +0200

    Small update for  blueprints

--- notes/routes.py
+++ notes/routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-
 from action_db import *
 
 notes_bp = Blueprint('notes', __name__, template_folder='templates')
